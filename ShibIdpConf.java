a6ec521fd03f7fe241b2360ddf46b3d32a56b6cc
hsimeoni <hsimeoni@gmail.com>
2013-12-18 13:29:17 +0100

    First Commit

---
+++ ShibIdpConf.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package shib.idp.conf;
+
+/**
+ *
+ * @author cosic_admin
+ */
+public class ShibIdpConf {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
